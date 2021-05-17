598e897dd510ee8624fde4dcc88e81bdcd91d128
Raghav <raghavarya2002@gmail.com>
2021-05-17 17:15:42 +0530

    Implementing the Firebase Music Source

---
+++ app/src/main/java/com/example/musicplayer/exoplayer/FirebaseMusicSource.kt
@@ -0,0 +1,43 @@
+package com.example.musicplayer.exoplayer
+
+class FirebaseMusicSource {
+
+    private val onReadyListeners = mutableListOf<(Boolean) -> Unit>()
+
+    private var state: State = State.STATE_CREATED
+        set(value) {
+            if (value == State.STATE_INITIALIZED || value == State.STATE_ERROR) {
+
+                synchronized(onReadyListeners) {
+                    field = value
+                    onReadyListeners.forEach { listener ->
+                        listener(state == State.STATE_INITIALIZED)
+                    }
+                }
+
+            } else {
+                field = value
+
+            }
+        }
+
+    fun whenReady(action: (Boolean) -> Unit): Boolean {
+
+        if (state == State.STATE_CREATED || state == State.STATE_INITIALIZED) {
+            onReadyListeners += action
+            return false
+        } else {
+            action(state == State.STATE_INITIALIZED)
+            return true
+        }
+
+    }
+
+}
+
+enum class State {
+    STATE_CREATED,
+    STATE_INITIALIZING,
+    STATE_INITIALIZED,
+    STATE_ERROR
+}
