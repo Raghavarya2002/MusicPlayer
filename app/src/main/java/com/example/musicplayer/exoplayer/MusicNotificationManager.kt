be1b402047d0e8aa4d55867839dab2d1fd474c0e
Raghav <raghavarya2002@gmail.com>
2021-05-21 21:49:20 +0530

    Setting up te music notification

---
+++ app/src/main/java/com/example/musicplayer/exoplayer/MusicNotificationManager.kt
@@ -0,0 +1,38 @@
+package com.example.musicplayer.exoplayer
+
+import android.content.Context
+import android.media.session.MediaSession
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.MediaSessionCompat
+import com.example.musicplayer.R
+import com.example.musicplayer.other.Constants.NOTIFICATION_CHANNEL_ID
+import com.example.musicplayer.other.Constants.NOTIFICATION_ID
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
+
+class MusicNotificationManager(
+    private val  context: Context,
+    sessionToken: MediaSessionCompat.Token,
+    notificationListener: PlayerNotificationManager.NotificationListener,
+    private val newSongCallback : () -> Unit
+ ) {
+
+    private val notificationManager : PlayerNotificationManager
+
+    init {
+        val mediaController = MediaControllerCompat(context, sessionToken)
+        notificationManager = PlayerNotificationManager.createWithNotificationChannel(
+            context,
+            NOTIFICATION_CHANNEL_ID,
+            R.string.notification_channel_name,
+            R.string.notification_channel_description,
+            NOTIFICATION_ID,
+            DescriptionAdapter(mediaController),
+            notificationListener
+
+        ).apply {
+            setSmallIcon(R.drawable.ic_music)
+            setMediaSessionToken(sessionToken)
+        }
+    }
+
+}
--- app/src/main/java/com/example/musicplayer/other/Constants.kt
+++ app/src/main/java/com/example/musicplayer/other/Constants.kt
@@ -3,4 +3,8 @@ package com.example.musicplayer.other
 object Constants {
 
     const val SONG_COLLECTION = "songs"
+
+    const val NOTIFICATION_CHANNEL_ID = "music"
+
+    const val NOTIFICATION_ID = 1
 }
