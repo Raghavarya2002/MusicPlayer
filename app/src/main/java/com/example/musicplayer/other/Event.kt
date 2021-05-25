ed25af411441aa3f33ddea24015d5b7b97da104b
Raghav <raghavarya2002@gmail.com>
2021-05-25 23:30:27 +0530

    Resouce &event class

--- app/src/main/java/com/example/musicplayer/other/Event.kt
+++ app/src/main/java/com/example/musicplayer/other/Event.kt
@@ -1,11 +1,18 @@
 package com.example.musicplayer.other
 
-open class Event<out T>(privata val data: T) {
+open class Event<out T>(private val data: T) {
 
     var hasBeenHandled = false
         private set
 
     fun getContentIfNotHandled(): T? {
-
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            data
+        }
     }
+
+    fun peekContent() = data
 }
