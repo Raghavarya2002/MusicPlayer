fe76915ab17a2237ac11d024364ddae32e7a9a6a
Raghav <raghavarya2002@gmail.com>
2021-05-29 23:29:25 +0530

    Music Service Connection

--- app/src/main/java/com/example/musicplayer/exoplayer/MusicServiceConnection.kt
+++ app/src/main/java/com/example/musicplayer/exoplayer/MusicServiceConnection.kt
@@ -1,8 +1,12 @@
 package com.example.musicplayer.exoplayer
 
+import android.content.ComponentName
 import android.content.Context
+import android.media.browse.MediaBrowser
+import android.media.session.MediaController
 import android.media.session.PlaybackState
 import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.PlaybackStateCompat
@@ -28,8 +32,53 @@ class MusicServiceConnection(context: Context) {
 
     lateinit var mediaController: MediaControllerCompat
 
+    private val mediaBrowserConnectionCallback = MediaBrowserConnectionCallback(context)
+
+    private val mediaBrowser = MediaBrowserCompat(
+        context,
+        ComponentName(
+            context,
+            MusicService::class.java,
+
+            ),
+        mediaBrowserConnectionCallback,
+        null
+    )
+
     val transportControls: MediaControllerCompat.TransportControls
         get() = mediaController.transportControls
+
+    private inner class MediaBrowserConnectionCallback(
+        private val context: Context
+    ) : MediaBrowserCompat.ConnectionCallback() {
+        override fun onConnected() {
+            mediaController = MediaControllerCompat(context, mediaBrowser.sessionToken).apply {
+                registerCallback(MediaControllerCallback())
+            }
+            _isConnected.postValue(Event(Resource.success(true)))
+
+        }
+
+        override fun onConnectionSuspended() {
+            _isConnected.postValue(
+                Event(
+                    Resource.error(
+                        "The connection was suspended", false
+                    )
+                )
+            )
+        }
+
+        override fun onConnectionFailed() {
+            _isConnected.postValue(
+                Event(
+                    Resource.error(
+                        "Couldn't connet to media browser", false
+                    )
+                )
+            )
+        }
+    }
 
     private inner class MediaControllerCallback : MediaControllerCompat.Callback() {
         override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
@@ -52,6 +101,10 @@ class MusicServiceConnection(context: Context) {
                     )
                 )
             }
+        }
+
+        override fun onSessionDestroyed() {
+            mediaBrowserConnectionCallback.onConnectionSuspended()
         }
     }
 }
