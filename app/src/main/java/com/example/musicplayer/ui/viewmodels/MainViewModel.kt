3c226441b257a95b5afd4cc58a06259efb5a8c1a
Raghav <raghavarya2002@gmail.com>
2021-05-30 23:26:50 +0530

    Main ViewModel

--- app/src/main/java/com/example/musicplayer/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/musicplayer/ui/viewmodels/MainViewModel.kt
@@ -1,17 +1,23 @@
 package com.example.musicplayer.ui.viewmodels
 
+import android.media.browse.MediaBrowser
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat.METADATA_KEY_MEDIA_ID
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.example.musicplayer.data.entities.Song
 import com.example.musicplayer.exoplayer.MusicServiceConnection
+import com.example.musicplayer.exoplayer.isPlayEnabled
+import com.example.musicplayer.exoplayer.isPlaying
+import com.example.musicplayer.exoplayer.isPrepared
 import com.example.musicplayer.other.Constants.MEDIA_ROOT_ID
 import com.example.musicplayer.other.Resource
 
 class MainViewModel @ViewModelInject constructor(
 
     private val musicServiceConnection: MusicServiceConnection
-) {
+) : ViewModel() {
 
     private val _mediaItems = MutableLiveData<Resource<List<Song>>>()
 
@@ -48,6 +54,47 @@ class MainViewModel @ViewModelInject constructor(
                     }
                     _mediaItems.postValue(Resource.success(items))
                 }
+            })
+    }
+
+    fun skipToNextSong() {
+        musicServiceConnection.transportControls.skipToNext()
+    }
+
+    fun skipToPreviousSong() {
+        musicServiceConnection.transportControls.skipToPrevious()
+    }
+
+
+    fun seekTo(pos: Long) {
+        musicServiceConnection.transportControls.seekTo(pos)
+    }
+
+    fun playOrToggleSong(mediaItem: Song, toggle: Boolean = false) {
+        val isPrepared = playbackState.value?.isPrepared ?: false
+        if (isPrepared && mediaItem.mediaId ==
+            curPlayingSong.value?.getString(METADATA_KEY_MEDIA_ID)
+        ) {
+            playbackState.value?.let { playbackState ->
+
+                when {
+                    playbackState.isPlaying -> if (toggle) musicServiceConnection.transportControls.pause()
+                    playbackState.isPlayEnabled -> musicServiceConnection.transportControls.play()
+                    else -> Unit
+                }
+            }
+        } else {
+            musicServiceConnection.transportControls.playFromMediaId(mediaItem.mediaId, null)
+        }
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        musicServiceConnection.unsubscribe(
+            MEDIA_ROOT_ID,
+            object : MediaBrowserCompat.SubscriptionCallback() {
+
             })
     }
 
